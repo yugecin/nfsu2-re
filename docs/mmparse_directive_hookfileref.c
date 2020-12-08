0ffd56f980c7d1f8b8949d5121b92fecd8f16a6b
yugecin <yugecin@users.noreply.github.com>
2020-12-08 20:02:34 +0100

    mmparse hookfileref: check file existence

--- docs/mmparse_directive_hookfileref.c
+++ docs/mmparse_directive_hookfileref.c
@@ -1,9 +1,18 @@
 static
 enum DIR_CONTENT_ACTION directive_hookfileref(char **to, char *from, struct DIRECTIVE *dir)
 {
+	FILE *f;
 	char filename[100];
+	char relfile[110];
 
 	get_directive_text(dir, filename);
+	sprintf(relfile, "../%s", filename);
+	f = fopen(relfile, "r");
+	if (f) {
+		fclose(f);
+	} else {
+		printf("line %d: file '%s' not found\n", current_line, filename);
+	}
 	*to += sprintf(*to,
 		"<code><a href='"
 		"https://github.com/yugecin/nfsu2-re/blob/master/%s"
